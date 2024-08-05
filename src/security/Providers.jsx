b18d6a5cadfc6580dde3b2217a59fb5bf6a59dfb
centinela2go <orlanditogg123456@gmail.com>
2024-08-05 01:02:13 -0500

    agregar host a login

--- src/security/Providers.jsx
+++ src/security/Providers.jsx
@@ -17,7 +17,7 @@ const AuthProvider = ({ children }) => {
 
   const loginAction = (data) => {
       
-      axios.post("http://127.0.0.1:8000/login/", data, {
+      axios.post("https://orlando.pythonanywhere.com/login/", data, {
         'Content-Type': 'application/json',
       }).then((res) => {
         if (res.data) {
